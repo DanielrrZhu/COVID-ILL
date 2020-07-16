decefdb0589122ee17c6e24a6ac0f189b07d0a7a
DanielrrZhu <danielzhu0310@hotmail.com>
2020-07-16 12:32:52 -0500

    build network

--- model.py
+++ model.py
@@ -18,16 +18,23 @@ tf.disable_v2_behavior()
 # Load data
 data_processing.download_data(download = False) #read preparition
 data_processing.Process_data(process = False)
-(CaseNum,Data) = data_processing.load_data()
+(CaseNum,Data_Load) = data_processing.load_data()
 data_processing.dailycasePlot(CaseNum, plot = False)
+# ========================================================================
+No = np.arange(np.size(Data_Load,0))
+Data = np.c_[No,Data_Load]
 
 np.random.shuffle(Data)
 sep = int(0.7*np.size(Data,0))
 train_data = Data[:sep]
 test_data = Data[sep:]
-# =============================================================================
-# Model
+# ========================================================================
+# Build network
 tf_input = tf.placeholder(tf.int32, [None,6], "input")
-tfx = tf_input[:, :5]
-tfy = tf_input[:, 5]
+tfx = tf_input[:, 1:6]
+tfy = tf_input[:, 6]
+
+l1 = tf.layers.dense(tfx, 128, tf.nn.relu, name="l1")
+l2 = tf.layers.dense(l1, 128, tf.nn.relu, name="l2")
+out = tf.layers.dense(l2, 1, name='l3')
 
